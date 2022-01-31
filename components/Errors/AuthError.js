8e3d8fd835958e9a09c6506040c4e9f7d60f590b
khggc <khggcvfd@gmail.com>
2022-02-01 02:50:10 +0500

    first commit

--- components/Errors/AuthError.js
+++ components/Errors/AuthError.js
@@ -1,14 +1,9 @@
-import React,{useEffect,useState} from 'react';
+import React from 'react';
 const AuthError = (props) =>{
     const borderColor = {
         'borderColor': props.res.status >= 250 ? '#FF004F' : '#2AC276',
         "display": props.res.text.length === 0 ? 'none' : 'block'
     }
-    const [err, setErr] = useState(props.res.text)
-    useEffect(() => {
-        setErr(props.res.text)
-    }, [props.res.text]);
-
     return(
         <span  className='auth-res-info' style={borderColor} >{props.res.text}</span>
     )
